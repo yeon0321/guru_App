d3565c36f6faa853e0d0592411a02ca2642c533d
byObin <byobin26@gmail.com>
2022-08-01 19:26:34 +0900

    hjy commit

--- app/src/main/java/com/example/guru_app/subActivity3.kt
+++ app/src/main/java/com/example/guru_app/subActivity3.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 
-class subActivity3 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class subActivity3 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
     lateinit var navigationView: NavigationView
     lateinit var drawerLayout: DrawerLayout
+
+    lateinit var preEx_body: Button
+    lateinit var preEx_upperbody: Button
+    lateinit var preEx_lowerbody: Button
+    lateinit var preEx_stretch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,36 @@ class subActivity3 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // 네비게이션 드로어 내에있는 화면의 이벤트를 처리하기 위해 생성
         navigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
+
+
+
+
+        preEx_body = findViewById(R.id.preEx_body)
+        preEx_upperbody = findViewById(R.id.preEx_upperbody)
+        preEx_lowerbody = findViewById(R.id.preEx_lowerbody)
+        preEx_stretch = findViewById(R.id.preEx_stretch)
+
+        preEx_body.setOnClickListener({
+            val intent_body = Intent(this, prepare_ex_body::class.java)
+            startActivity(intent_body)
+        })
+
+
+        preEx_lowerbody.setOnClickListener({
+            val intent_lowerbody = Intent(this, prepare_ex_lowerbody::class.java)
+            startActivity(intent_lowerbody)
+        })
+
+        preEx_upperbody.setOnClickListener({
+            val intent_upperbody = Intent(this, prepare_ex_upperbody::class.java)
+            startActivity(intent_upperbody)
+        })
+
+        preEx_stretch.setOnClickListener({
+            val intent_stretch = Intent(this, prepare_ex_stretching::class.java)
+            startActivity(intent_stretch)
+        })
+
 
     }
 
@@ -38,7 +74,10 @@ class subActivity3 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 val intent2 = Intent(this, eqList::class.java)
                 startActivity(intent2)
             }
-            R.id.menu3_preEx-> Toast.makeText(this,"현재 페이지입니다.",Toast.LENGTH_SHORT).show()
+            R.id.menu3_preEx-> {
+                val intent3 = Intent(this, subActivity3::class.java)
+                startActivity(intent3)
+            }
             R.id.menu4_exDiary-> {
                 val intent4 = Intent(this, subActivity4::class.java)
                 startActivity(intent4)
--- app/src/main/java/com/example/guru_app/subActivity4.kt
+++ app/src/main/java/com/example/guru_app/subActivity4.kt
@@ -1,22 +1,45 @@
 package com.example.guru_app;
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.guru_app.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
+import com.prolificinteractive.materialcalendarview.*
+import kotlinx.android.synthetic.main.ex_diary.*
+import java.util.*
 
 
 class subActivity4 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var navigationView: NavigationView
     lateinit var drawerLayout: DrawerLayout
+    lateinit var materialCalendarView: MaterialCalendarView
+    lateinit var addDiaryBtn: Button
+    lateinit var selctedDate: TextView
+    lateinit var edit_text: TextView
+    private lateinit var binding: ActivityMainBinding
+    private val context = this
+    //lateinit var dBmanager: DBmanager
+    lateinit var sqlitedb: SQLiteDatabase
+    lateinit var registDiary: Button
+    lateinit var showDiary : TextView
+    lateinit var content : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ex_diary)
+
+        selctedDate = findViewById(R.id.selected_date)
+        edit_text = findViewById(R.id.edit_text)
+        showDiary = findViewById(R.id.saved_diary)
 
         // 네비게이션 드로어 생성
         drawerLayout = findViewById(R.id.drawer_layout)
@@ -25,7 +48,59 @@ class subActivity4 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
 
+
+
+        materialCalendarView = findViewById(R.id.materialCalendar)
+        materialCalendarView.setSelectedDate(CalendarDay.today()) //처음 실행했을 때 오늘 날짜 선택된 상태
+        materialCalendarView.addDecorator(TodayDecorator()) // 오늘 날짜 글자 색 바꿈
+        materialCalendarView.setOnDateChangedListener(object: OnDateSelectedListener { //클릭하면 닷 추가
+            override fun onDateSelected(widget: MaterialCalendarView, date: CalendarDay, selected: Boolean) {
+                materialCalendarView.addDecorator(EventDecorator(Collections.singleton(date)))
+                val selcteDate : String
+                selctedDate.setText(materialCalendarView.selectedDate.toString())
+                var cursor : Cursor
+            }
+
+        })
+
+
+
+
+
+
+        addDiaryBtn = findViewById(R.id.add_diary_btn)
+
+
+        addDiaryBtn.setOnClickListener {
+            val dialog = CustomDialog(this)
+            dialog.showDialog()
+            dialog.setOnClickListener(object : CustomDialog.OnDialogClickListener {
+                override fun onClicked(name: String)
+                {
+                    edit_text.setText(name)
+                }
+
+            })
+        }
+
+        registDiary = findViewById(R.id.regi_diary_btn)
+
+       // dBmanager = DBmanager(this,"diaryDB",null,1)
+
+        registDiary.setOnClickListener {
+           //sqlitedb = dBmanager.writableDatabase
+            sqlitedb.execSQL("INSERT INTO diary VALUES('"+selctedDate+"', '"+edit_text+"')")
+            sqlitedb.close()
+        }
+
+
+
+
+
+
     }
+
+
 
     // 드로어 내 아이템 클릭 이벤트 처리하는 함수
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -42,8 +117,14 @@ class subActivity4 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 val intent3 = Intent(this, subActivity3::class.java)
                 startActivity(intent3)
             }
-            R.id.menu4_exDiary-> Toast.makeText(this,"현재 페이지입니다.",Toast.LENGTH_SHORT).show()
+            R.id.menu4_exDiary-> {
+                val intent4 = Intent(this, subActivity4::class.java)
+                startActivity(intent4)
+            }
         }
         return false
+
     }
+
+
 }
